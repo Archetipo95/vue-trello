d0413dee9717606df0a8668b54375e11373de798
Archetipo95 <martin.masevski@gmail.com>
2021-06-30 15:35:46 +0200

    Added drag and drop for single tasks

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -3,13 +3,13 @@
     <div class="flex flex-row items-start">
       <div
         class="column"
-        v-for="(column, columnIndex) in board.columns"
-        :key="columnIndex"
-        @drop="dropDownTaskOrColumn($event, column.tasks, columnIndex)"
+        v-for="(column, $columnIndex) of board.columns"
+        :key="$columnIndex"
+        draggable
+        @drop="moveTaskOrColumn($event, column.tasks, $columnIndex)"
         @dragover.prevent
         @dragenter.prevent
-        draggable
-        @dragstart.self="pickUpColumn($event, columnIndex)"
+        @dragstart.self="pickupColumn($event, $columnIndex)"
       >
         <div class="flex items-center mb-2 font-bold">
           {{ column.name }}
@@ -17,11 +17,16 @@
         <div class="list-reset">
           <div
             class="task"
-            v-for="task in column.tasks"
-            :key="task.id"
+            v-for="(task, $taskIndex) of column.tasks"
+            :key="$taskIndex"
             draggable
-            @dragstart="pickUpTask($event, task.id, columnIndex)"
+            @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
             @click="goToTask(task)"
+            @dragover.prevent
+            @dragenter.prevent
+            @drop.stop="
+              moveTaskOrColumn($event, column.tasks, $columnIndex, $taskIndex)
+            "
           >
             <span class="w-full flex-no-shrink font-bold">
               {{ task.name }}
@@ -74,37 +79,53 @@ export default {
       })
       e.target.value = ''
     },
-    pickUpTask(e, taskId, fromColumnIndex) {
+    pickupTask(e, taskIndex, fromColumnIndex) {
       e.dataTransfer.effectAllowed = 'move'
       e.dataTransfer.dropEffect = 'move'
 
-      e.dataTransfer.setData('task-id', taskId)
+      e.dataTransfer.setData('from-task-index', taskIndex)
       e.dataTransfer.setData('from-column-index', fromColumnIndex)
       e.dataTransfer.setData('type', 'task')
     },
-    pickUpColumn(e, fromColumnIndex) {
+    pickupColumn(e, fromColumnIndex) {
       e.dataTransfer.effectAllowed = 'move'
       e.dataTransfer.dropEffect = 'move'
+
       e.dataTransfer.setData('from-column-index', fromColumnIndex)
       e.dataTransfer.setData('type', 'column')
     },
-    dropDownTaskOrColumn(e, toTasks, toColumnIndex) {
+    moveTaskOrColumn(e, toTasks, toColumnIndex, toTaskIndex) {
       const type = e.dataTransfer.getData('type')
+
       if (type === 'task') {
-        this.dropDownTask(e, toTasks)
+        this.moveTask(
+          e,
+          toTasks,
+          toTaskIndex !== undefined ? toTaskIndex : toTasks.length
+        )
       } else {
-        this.dropDownColumn(e, toColumnIndex)
+        this.moveColumn(e, toColumnIndex)
       }
     },
-    dropDownTask(e, toTasks) {
+    moveTask(e, toTasks, toTaskIndex) {
       const fromColumnIndex = e.dataTransfer.getData('from-column-index')
       const fromTasks = this.board.columns[fromColumnIndex].tasks
-      const taskIndex = e.dataTransfer.getData('task-id')
-      this.$store.commit('MOVE_TASK', { fromTasks, toTasks, taskIndex })
+      const fromTaskIndex = e.dataTransfer.getData('from-task-index')
+
+      this.$store.commit('MOVE_TASK', {
+        fromTasks,
+        fromTaskIndex,
+        toTasks,
+        toTaskIndex
+      })
     },
-    dropDownColumn(e, toColumnIndex) {
+    moveColumn(e, toColumnIndex) {
       const fromColumnIndex = e.dataTransfer.getData('from-column-index')
-      this.$store.commit('MOVE_COLUMN', { fromColumnIndex, toColumnIndex })
+
+      this.$store.commit('MOVE_COLUMN', {
+        fromColumnIndex,
+        toColumnIndex
+      })
     }
   }
 }
@@ -126,6 +147,6 @@ export default {
 
 .task-bg {
   @apply pin absolute;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0,0,0,0.5);
 }
 </style>
